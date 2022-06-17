406c83d407ac3be0debac9ffe2221e7fd6452d37
William Smith <mr.williamjdsmith@gmail.com>
2022-06-16 19:34:39 -0700

    Update python_solutions.py
    Updated the structure for the entry comment

--- python_solutions.py
+++ python_solutions.py
@@ -1,4 +1,9 @@
-# Kata: Multiples of 3 or 5
+""" 
+06/16/22
+Kata: Multiples of 3 or 5
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
+Additionally, if the number is negative, return 0 (for languages that do have them).
+"""
 def solution(number):
     if number < 0:
         return 0
