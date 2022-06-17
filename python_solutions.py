03e5d9f87fc1f80eda2073e19158941773a22ccc
William Smith <mr.williamjdsmith@gmail.com>
2022-06-16 18:40:58 -0700

    Create python_solutions.py

---
+++ python_solutions.py
@@ -0,0 +1,17 @@
+# Kata: Multiples of 3 or 5
+# Solution:
+def solution(number):
+    if number < 0:
+        return 0
+    
+    multiples = []
+    i = 1
+    while i < number:
+        if i % 3 == 0 or i % 5 == 0:
+            multiples.append(i)
+        i += 1
+        
+    total = 0
+    for m in multiples:
+        total += m
+    return total
