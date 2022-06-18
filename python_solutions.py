cb7ab42a194fcc08ab5837160f38f12fe819dba3
William Smith <mr.williamjdsmith@gmail.com>
2022-06-18 09:22:52 -0700

    Added Kata: Isograms
    Added new challenge and changed comment structure again

--- python_solutions.py
+++ python_solutions.py
@@ -1,5 +1,4 @@
 """ 
-06/16/22
 Kata: Multiples of 3 or 5
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
 Additionally, if the number is negative, return 0 (for languages that do have them).
@@ -19,3 +18,18 @@ def solution(number):
     for m in multiples:
         total += m
     return total
+
+"""
+Kata: Isograms
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. 
+Assume the empty string is an isogram. Ignore letter case.
+"""
+def is_isogram(string):
+    letters = []   # holds letters to check for duplicates
+    for s in string.lower():   # checks every letter in lowercase to make them uniform
+        if s not in letters:   # compares letters with duplicate list
+            letters.append(s)   # adds safe letters to be checked in list
+        else:
+            return False   # immediately ends the progam for any duplicate
+    return True
