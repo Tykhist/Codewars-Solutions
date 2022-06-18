3ae587381682af4c4ff138e810f1a81b6afc87d3
William Smith <mr.williamjdsmith@gmail.com>
2022-06-18 10:00:30 -0700

    Added Kata: Complementary DNA
    Also added ranks/kyu to journaling comments, and decided to add almost the entire challenge description instead of only the immediately relevant parts. Will also bring this change to the Go file

--- python_solutions.py
+++ python_solutions.py
@@ -1,7 +1,9 @@
 """ 
 Kata: Multiples of 3 or 5
+Rank: 6 kyu
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
-Additionally, if the number is negative, return 0 (for languages that do have them).
+Additionally, if the number is negative, return 0. If the number is a multiple of both 3 and 5, only count it once.
 """
 def solution(number):
     if number < 0:
@@ -21,6 +23,7 @@ def solution(number):
 
 """
 Kata: Isograms
+Rank: 7 kyu
 An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
 Implement a function that determines whether a string that contains only letters is an isogram. 
 Assume the empty string is an isogram. Ignore letter case.
@@ -33,3 +36,22 @@ def is_isogram(string):
         else:
             return False   # immediately ends the progam for any duplicate
     return True
+
+"""
+Kata: Complementary DNA
+Rank: 7 kyu
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the 
+development and functioning of living organisms. If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+Your function receives one side of the DNA; you need to return the other complementary side. 
+DNA strand is never empty or there is no DNA at all.
+"""
+def DNA_strand(dna):
+    result = ""   # initializes a string to return
+    for i in dna.upper():   # ensures that all letters are uniform
+        if i == "A": result += "T"   # builds the return string
+        elif i == "C": result += "G"
+        elif i == "T": result += "A"
+        elif i == "G": result += "C"
+    return result
