c03c1ad5cd60a326ffbbb1a9ee018c20223b3dae
William Smith <mr.williamjdsmith@gmail.com>
2022-06-18 12:27:42 -0700

    Added Kata: Playing with digits

--- python_solutions.py
+++ python_solutions.py
@@ -55,3 +55,38 @@ def DNA_strand(dna):
         elif i == "T": result += "A"
         elif i == "G": result += "C"
     return result
+
+"""
+Kata: Playing with digits
+Some numbers have funny properties. For example:
+89 --> 8¹ + 9² = 89 * 1
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p,
+we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the 
+successive powers of p is equal to k * n. In other words:
+
+Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+If it is the case we will return k, if not return -1.
+Note: n and p will always be given as strictly positive integers.
+"""
+def dig_pow(n, p):
+    """
+    1. Split n into digits
+    2. Give each digit an exponent, starting with p and increasing by 1
+    3. Add these new numbers together
+    4. Divide by n
+    5. Return quotient if it is an integer
+    6. Return -1 otherwise
+    """ 
+    digits = list(str(n))
+
+    exponents = []
+    for i in range(len(digits)):
+        exponents.append(int(digits[i])**(p+i))
+
+    if sum(exponents) % n == 0:
+        return sum(exponents) / n
+
+    return -1
