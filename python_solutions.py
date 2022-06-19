90e6de9a45c882af82870eda4f084903fa957c6f
William Smith <mr.williamjdsmith@gmail.com>
2022-06-19 11:05:16 -0700

    Added Kata: List Filtering

--- python_solutions.py
+++ python_solutions.py
@@ -130,3 +130,12 @@ def wave(people):
         ))
     return results
     """
+
+"""
+Kata: List Filtering
+Rank: 7 kyu
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+"""
+def filter_list(l):
+    return [x for x in l if type(x) == type(int())]
