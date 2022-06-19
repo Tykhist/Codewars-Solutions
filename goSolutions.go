b8bac42cdad7f4f247b36cdb8113d33c67d2033a
William Smith <mr.williamjdsmith@gmail.com>
2022-06-19 11:56:02 -0700

    Added Kata: Even or Odd

--- goSolutions.go
+++ goSolutions.go
@@ -20,3 +20,18 @@ func Multiple3And5(number int) int {
   }
   return result
 }
+
+/* 
+Kata: Even or Odd
+Rank: 8 kyu
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+package kata
+
+func EvenOrOdd(number int) string {
+  if number % 2 == 0 {
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
