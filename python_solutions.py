b496df1f440b5cd094b9a772319472fab0dbe4b6
William Smith <mr.williamjdsmith@gmail.com>
2022-06-27 14:51:06 -0700

    Added Kata: Stop gninnipS My sdroW!

--- python_solutions.py
+++ python_solutions.py
@@ -246,4 +246,24 @@ def reverse_words(text):
     """ Joins list of reversed words into one string """
     return "".join(result)
             
-            
+"""
+Kata: Stop gninnipS My sdroW!
+Rank: 6 kyu
+Write a function that takes in a string of one or more words, and returns the same string, 
+but with all five or more letter words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+"""
+# Note that since the last kata, I realized that a word can be reversed simply by using word[::-1]
+def spin_words(sentence):
+    result = []
+    words = sentence.split()
+    for letter in words:
+        if len(letter) >= 5:
+            result.append(letter[::-1])
+        else:
+            result.append(letter)
+    return " ".join(result)
