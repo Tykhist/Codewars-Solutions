5788ca787591baad70cec7adec2693e8cfb95b8f
William Smith <mr.williamjdsmith@gmail.com>
2022-06-28 20:07:59 -0700

    Added Kata: Square(n) Sum

--- python_solutions.py
+++ python_solutions.py
@@ -308,3 +308,13 @@ def caffeine(coffee, number):
     elif caff == 4: return "You can have 2 more shots then no more!"
     elif caff == 5: return "You can only have an espresso, latte or a decaf now"
     elif caff >= 6: return "Only decaf for you now!"
+
+"""
+Kata: Square(n) Sum
+Rank: 8 kyu
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+"""
+def square_sum(numbers):
+    result = [i ** 2 for i in numbers]
+    return sum(result)
