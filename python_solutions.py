201bfed3f048f5a5e691b2481efe021b57359118
William Smith <mr.williamjdsmith@gmail.com>
2022-06-28 20:13:12 -0700

    Added Kata: Friend or Foe?

--- python_solutions.py
+++ python_solutions.py
@@ -318,3 +318,13 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 def square_sum(numbers):
     result = [i ** 2 for i in numbers]
     return sum(result)
+
+"""
+Kata: Friend or Foe?
+Rank: 7 kyu
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+"""
+def friend(x):
+    return [i for i in x if len(i) == 4]
