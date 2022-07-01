188c46985fc33aa6109a6fbc3d9a7b93934dab76
William Smith <mr.williamjdsmith@gmail.com>
2022-07-01 16:42:54 -0700

    Added Kata: Unique In Order

--- python_solutions.py
+++ python_solutions.py
@@ -328,3 +328,18 @@ Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 """
 def friend(x):
     return [i for i in x if len(i) == 4]
+
+"""
+Kata: Unique In Order
+Rank: 6 kyu
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items
+without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1,2,2,3,3])       == [1,2,3]
+"""
+def unique_in_order(iterable):
+    if iterable == None: return iterable
+    return [iterable[i] for i in range(len(iterable)-1) if iterable[i] != iterable[i+1]] + [iterable[-1] if len(iterable) > 0 else None]
