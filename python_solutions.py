85db4d4c107c0195b02c305f4e53494554f85e0f
William Smith <mr.williamjdsmith@gmail.com>
2022-07-02 22:17:14 -0700

    Added Kata: Two to One

--- python_solutions.py
+++ python_solutions.py
@@ -376,3 +376,20 @@ def printer_error(s):
         if letter not in good_letters:
             count += 1 
     return f"{count}/{len(s)}"
+
+"""
+Kata: Two to One
+Rank: 7 kyu
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the 
+longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+"""
+def longest(a1, a2):
+    return ''.join(sorted(set(a1+a2)))
