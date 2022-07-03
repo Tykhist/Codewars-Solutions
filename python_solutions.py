f97fdc8f11bc6c06dd91414c0ba7dda03201c284
William Smith <mr.williamjdsmith@gmail.com>
2022-07-03 01:03:07 -0700

    Added Kata: Sort the odd

--- python_solutions.py
+++ python_solutions.py
@@ -455,3 +455,22 @@ def find_uniq(arr):
     for i in range(len(arr)):
         if arr[i] != arr[i-1] and arr[i] != arr[i-2]:
             return arr[i]
+
+"""
+Kata: Sort the odd
+Rank: 6 kyu
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+"""
+def sort_array(source_array):
+    odd = sorted([i for i in source_array if i % 2 == 1])
+    count = 0
+    for i in range(len(source_array)):
+        if source_array[i] % 2 == 1:
+            source_array[i] = odd[count]
+            count += 1
+    return source_array
