223669753d667f87cdd2949409b032e7755ebb3c
William Smith <mr.williamjdsmith@gmail.com>
2022-07-03 00:15:43 -0700

    Added Kata: Range Extraction

--- python_solutions.py
+++ python_solutions.py
@@ -393,3 +393,36 @@ longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 """
 def longest(a1, a2):
     return ''.join(sorted(set(a1+a2)))
+
+"""
+Kata: Range Extraction
+Rank: 4 kyu
+A format for expressing an ordered list of integers is to use a comma separated list of either
+
+individual integers
+
+or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all 
+integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+
+Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+Example:
+solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+"""
+def solution(args):
+    new = []
+    for i in range(len(args[1:])):
+        if args[i+1] == args[i]+1 and args[i-1] == args[i]-1:
+            new.append('-')
+        else:
+            new.append(str(args[i]))
+    new.append(str(args[-1]))
+    
+    new = [new[i] for i in range(len(new[:-1])) if new[i] != new[i+1]]
+    new.append(str(args[-1]))
+    
+    new = ','.join(new)
+    new = new.replace(',-,', '-')
+    
+    return new
