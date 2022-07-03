d02ea4aed0bac0f3aa2d9f837bb45760c76cf75a
William Smith <mr.williamjdsmith@gmail.com>
2022-07-03 00:48:04 -0700

    Added Kata: Find the unique number

--- python_solutions.py
+++ python_solutions.py
@@ -439,3 +439,19 @@ Note: for this kata y isn't considered a vowel.
 def disemvowel(string_):
     string_ = [letter for letter in string_ if letter not in "aeiouAEIOU"]
     return ''.join(string_)
+
+"""
+Kata: Find the unique number
+Rank: 6 kyu
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+"""
+def find_uniq(arr):
+    for i in range(len(arr)):
+        if arr[i] != arr[i-1] and arr[i] != arr[i-2]:
+            return arr[i]
