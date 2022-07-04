090f135ca0b4d1b80ba84e39a38949fd0b705dc9
William Smith <mr.williamjdsmith@gmail.com>
2022-07-04 15:47:07 -0700

    Added Kata: Mumbling

--- python_solutions.py
+++ python_solutions.py
@@ -488,3 +488,23 @@ def find_short(s):
         if len(i) < len(l):
             l = i
     return len(l) # l: shortest word length
+
+"""
+Kata: Mumbling
+Rank: 7 kyu
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+def accum(s):
+    count = 0
+    result = []
+    for i in s.lower():
+        result.append(i.upper() + i * count)
+        count += 1
+    return '-'.join(result)
