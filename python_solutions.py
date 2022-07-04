82eeb2ae21da5cfee3f51a4a51d97e35dab3148b
William Smith <mr.williamjdsmith@gmail.com>
2022-07-04 15:58:34 -0700

    Added Kata: Sum of two lowest positive integers

--- python_solutions.py
+++ python_solutions.py
@@ -508,3 +508,16 @@ def accum(s):
         result.append(i.upper() + i * count)
         count += 1
     return '-'.join(result)
+
+"""
+Kata: Sum of two lowest positive integers
+Rank: 7 kyu
+Create a function that returns the sum of the two lowest positive numbers given an array 
+of minimum 4 positive integers. No floats or non-positive integers will be passed.
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+"""
+def sum_two_smallest_numbers(numbers):
+    result = sorted([i for i in numbers if i >= 0])
+    return result[0] + result[1]
