d2421b55e65ad60b0bd226700952e7be5304ce0a
William Smith <mr.williamjdsmith@gmail.com>
2022-07-06 12:30:37 -0700

    Added Kata: Even or Odd

--- Go-Solutions/rank-8-kyu.go
+++ Go-Solutions/rank-8-kyu.go
@@ -1,1 +1,14 @@
+/* 
+Kata: Even or Odd
+Rank: 8 kyu
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+package kata
 
+func EvenOrOdd(number int) string {
+  if number % 2 == 0 {
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
