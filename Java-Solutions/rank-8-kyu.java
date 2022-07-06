2438b6b6638d74b817152cb3802a60c5b09711ec
William Smith <mr.williamjdsmith@gmail.com>
2022-07-06 12:52:13 -0700

    Create rank-8-kyu.java

---
+++ Java-Solutions/rank-8-kyu.java
@@ -0,0 +1,10 @@
+/*
+Kata: You Can't Code Under Pressure #1
+Rank: 8 kyu
+Code as fast as you can! You need to double the integer and return it.
+*/
+class Java {
+  public static int doubleInteger(int i) {
+    return 2*i;
+  }
+}
