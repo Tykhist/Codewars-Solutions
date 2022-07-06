fa9e1f30e1456b67cf997ff8066ea50219d0b502
William Smith <mr.williamjdsmith@gmail.com>
2022-07-06 12:45:15 -0700

    Update python_solutions.py

--- python_solutions.py
+++ python_solutions.py
@@ -206,16 +206,6 @@ def caffeine(coffee, number):
     elif caff >= 6: return "Only decaf for you now!"
 
 """
-Kata: Square(n) Sum
-Rank: 8 kyu
-Complete the square sum function so that it squares each number passed into it and then sums the results together.
-For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
-"""
-def square_sum(numbers):
-    result = [i ** 2 for i in numbers]
-    return sum(result)
-
-"""
 Kata: Unique In Order
 Rank: 6 kyu
 Implement the function unique_in_order which takes as argument a sequence and returns a list of items
