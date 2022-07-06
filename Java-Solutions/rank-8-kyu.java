e07e8222e3a3b2627d855430c491570324b8e8eb
William Smith <mr.williamjdsmith@gmail.com>
2022-07-06 14:30:39 -0700

    Added Kata: Basic Mathematical Operations

--- Java-Solutions/rank-8-kyu.java
+++ Java-Solutions/rank-8-kyu.java
@@ -8,3 +8,39 @@ class Java {
     return 2*i;
   }
 }
+
+/*
+Kata: Basic Mathematical Operations
+Rank: 8 kyu
+Your task is to create a function that does four basic mathematical operations.
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    int num = 0;
+    switch (op) {
+        case "+":
+          num = v1 + v2;
+          break;
+        case "-":
+          num = v1 - v2;
+          break;
+        case "*":
+          num = v1 * v2;
+          break;
+        case "/":
+          num = v1 / v2;
+          break;
+    }
+    return num;
+  }
+}
