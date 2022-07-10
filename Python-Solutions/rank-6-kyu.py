5d77e8c7e78db553e48f60473415d700faa069ac
William Smith <mr.williamjdsmith@gmail.com>
2022-07-10 16:28:51 -0700

    Added Kata: Consecutive strings

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -196,3 +196,40 @@ def count_bits(n):
         if i == "1":
             result += 1
     return result
+
+"""
+Kata: Consecutive strings
+Rank: 6 kyu
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+consecutive strings : follow one after another without an interruption
+"""
+def longest_consec(strarr, k):
+    combos = []
+    for i in range(len(strarr)):
+        temp = ""
+        for j in strarr[i:i+k]:
+            temp += j
+        combos.append(temp)
+    combos = sorted(combos, key=lambda x: len(x), reverse=True)
+    return combos[0] if k > 0 and k <= len(strarr) else ""
