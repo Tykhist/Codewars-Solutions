e6e23e5894e273f69b688684c7f46dee4b237cab
William Smith <mr.williamjdsmith@gmail.com>
2022-07-10 10:41:11 -0700

    Update rank-7-kyu.py

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -309,3 +309,23 @@ def get_sum(a,b):
         result += i
     return a if a == b else result
 
+"""
+Kata: Sum of the first nth term of Series
+Rank: 7 kyu
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+If the given value is 0 then it should return 0.00
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+"""
+def series_sum(n):
+    series = [(1/(1+i*3)) for i in range(n)]
+    return f"{sum(series):.2f}"
