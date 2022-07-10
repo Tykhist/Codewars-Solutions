eb7bfeedf95c0df8bc1400c6d1c52f807607be24
William Smith <mr.williamjdsmith@gmail.com>
2022-07-10 13:35:52 -0700

    Added Kata: Bit Counting

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -183,3 +183,16 @@ def sort_array(source_array):
             count += 1
     return source_array
 
+"""
+Kata: Bit Counting
+Rank: 6 kyu
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+"""
+def count_bits(n):
+    result = 0
+    for i in f"{bin(n)}":
+        if i == "1":
+            result += 1
+    return result
