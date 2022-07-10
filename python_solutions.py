c8ca202dae469e81939dcbccc2b0e7a74b8e5863
William Smith <mr.williamjdsmith@gmail.com>
2022-07-10 10:57:58 -0700

    Added Kata: Simple Pig Latin

--- python_solutions.py
+++ python_solutions.py
@@ -102,3 +102,19 @@ def solution(args):
     
     return new
 
+"""
+Kata: Simple Pig Latin
+Rank: 5 kyu
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+"""
+def pig_it(text):
+    text = text.split()
+    result = []
+    for i in text:
+        if i.isalpha(): result.append(i[1:] + i[0] + 'ay') 
+        else: result.append(i)
+    return " ".join(result)
