b8c35e60fdbca44fc9ab9cdef53a91c149a85028
William Smith <mr.williamjdsmith@gmail.com>
2022-07-22 11:14:17 -0700

    Added Kata: Find the next perfect square!

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -394,3 +394,26 @@ def open_or_senior(data):
         else:
             output.append("Open")
     return output
+
+"""
+Kata: Find the next perfect square!
+Rank: 7 kyu
+You might know some pretty large perfect squares. But what about the NEXT one? Complete the findNextSquare 
+method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral 
+perfect square is an integer n such that sqrt(n) is also an integer. If the parameter is itself not a perfect 
+square then -1 should be returned. You may assume the parameter is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+"""
+import math
+
+def find_next_square(sq):
+    square = math.sqrt(sq)
+    return (square+1)**2 if square == int(square) else -1
+
+
+
