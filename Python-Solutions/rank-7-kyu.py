82a13f64852f7faac3523f234fe6a54bc54afd27
William Smith <mr.williamjdsmith@gmail.com>
2022-07-21 20:16:33 -0700

    Added Kata: Categorize New Member

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -366,5 +366,31 @@ However, the arrays can have varying lengths, not just limited to 4.
 def binary_array_to_number(arr):
     return int('0b' + ''.join([str(i) for i in arr]), 2)
 
+"""
+Kata: Categorize New Member
+Rank: 7 kyu
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help 
+with an application form that will tell prospective members which category they will be placed.
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet 
+club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
+Input:
+Input will consist of a list of pairs. Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
 
+Output:
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the 
+respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+"""
+def open_or_senior(data):
+    output = []
+    for i in data:
+        if i[0] >= 55 and i[1] >7:
+            output.append("Senior")
+        else:
+            output.append("Open")
+    return output
