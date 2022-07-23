db68e5c442856838f524e9fc1f366d6686d9f424
William Smith <mr.williamjdsmith@gmail.com>
2022-07-22 22:23:42 -0700

    Added Kata: Replace With Alphabet Position

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -256,3 +256,21 @@ def solution(s):
             result.append(temp)
             temp = i
     return " ".join(result + [temp])
+
+"""
+Kata: Replace With Alphabet Position
+Rank: 6 kyu
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabet_position("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+"""
+def alphabet_position(text):
+    return " ".join(f"{int(i, 36) - 9}" for i in text if i.isalpha())
