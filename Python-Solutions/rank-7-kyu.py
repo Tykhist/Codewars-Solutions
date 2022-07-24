5e3d695fd35ead078966605724dcd1c9f1ee4423
William Smith <mr.williamjdsmith@gmail.com>
2022-07-23 20:38:58 -0700

    Added Kata: Find the stray number

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -434,3 +434,17 @@ def validate_pin(pin):
         else: return False
     return True
 
+"""
+Kata: Find the stray number
+Rank: 7 kyu
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+Complete the method which accepts such an array, and returns that single different number.
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+"""
+def stray(arr):
+    return [i for i in arr if arr.count(i) == 1][0]
+    
