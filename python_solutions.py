7dcb0743819eec0203da49cd3d95808a4b64fb66
William Smith <mr.williamjdsmith@gmail.com>
2022-07-24 19:42:40 -0700

    Added Kata: Moving Zeros To The End

--- python_solutions.py
+++ python_solutions.py
@@ -185,3 +185,16 @@ def dirReduc(arr):
         arr = temp("WEST", "EAST")
         
     return arr
+
+"""
+Kata: Moving Zeros To The End
+Rank: 5 kyu
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+def move_zeros(lst):
+    for i in lst:
+        if i == 0:
+            lst.remove(i)
+            lst.append(0)
+    return lst
