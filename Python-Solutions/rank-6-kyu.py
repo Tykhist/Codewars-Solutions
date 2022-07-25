f931969fa109ea297065d46db9c69ffc4a6545a5
William Smith <mr.williamjdsmith@gmail.com>
2022-07-24 20:23:01 -0700

    Added Kata: Sum of Digits / Digital Root

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -274,3 +274,21 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 """
 def alphabet_position(text):
     return " ".join(f"{int(i, 36) - 9}" for i in text if i.isalpha())
+
+"""
+Kata: Sum of Digits / Digital Root
+Rank: 6 kyu
+Digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. 
+If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+"""
+def digital_root(n):
+    if n >= 10:
+        n = digital_root( sum( [int(i) for i in str(n)] ) )
+    return n
