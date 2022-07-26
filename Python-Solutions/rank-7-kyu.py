09703de3e28c1df2339b6979d60709cb25d880b3
William Smith <mr.williamjdsmith@gmail.com>
2022-07-25 19:36:54 -0700

    Added Kata: Highest and Lowest

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -448,3 +448,21 @@ Examples
 def stray(arr):
     return [i for i in arr if arr.count(i) == 1][0]
     
+"""
+Kata: Highest and Lowest
+Rank: 7 kyu
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+high_and_low("1 2 3 4 5")  # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"
+
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+"""
+def high_and_low(numbers):
+    numbers = [int(i) for i in numbers.split()]
+    return f"{max(numbers)} {min(numbers)}"
