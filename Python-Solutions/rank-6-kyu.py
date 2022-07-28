591917dde444e9308df39db77064ae6761f15d87
William Smith <mr.williamjdsmith@gmail.com>
2022-07-27 22:13:16 -0700

    Added Kata: Split Strings

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -292,3 +292,17 @@ def digital_root(n):
     if n >= 10:
         n = digital_root( sum( [int(i) for i in str(n)] ) )
     return n
+
+"""
+Kata: Split Strings
+Rank: 6 kyu
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an 
+odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+"""
+def solution(s):
+    if len(s) % 2: s += "_"
+    return [s[i-1] + s[i] for i in range(1, len(s), 2)] if s else []
