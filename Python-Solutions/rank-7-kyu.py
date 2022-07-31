f73d692160fcac78ec513e580f9c41ea7336d75a
William Smith <mr.williamjdsmith@gmail.com>
2022-07-30 21:45:04 -0700

    Added Kata: Colour Association

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -491,3 +491,18 @@ vowelOne "aeiou, abc" -- "1111100100"
 """
 def vowel_one(s):
     return "".join(['1' if i.lower() in "aeiou" else '0' for i in s])
+
+"""
+Kata: Colour Association
+Rank: 7 kyu
+Colour plays an important role in our lifes. Most of us like this colour better then another. User 
+experience specialists believe that certain colours have certain psychological meanings for us.
+You are given a 2D array, composed of a colour and its 'common' association in each array element. 
+The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+For example:
+var array = [["white", "goodness"], ...] returns [{'white': 'goodness'}, ...]
+"""
+def colour_association(arr):
+    return [{color[0]: color[1]} for color in arr]
+
