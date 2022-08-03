0df2dbe305263cd10f1cf1ec1151f565ca110e21
William Smith <mr.williamjdsmith@gmail.com>
2022-08-02 23:07:33 -0700

    Added Kata: Love vs friendship

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -541,3 +541,22 @@ All inputs will be valid.
 """
 def digits(n):
     return len(str(n))
+
+"""
+Kata: Love vs friendship
+Rank: 7 kyu
+If　a = 1, b = 2, c = 3 ... z = 26
+Then l + o + v + e = 54
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+The input will always be in lowercase and never be empty.
+"""
+def words_to_marks(s):
+    dict = {}
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    for i in range(26):
+        dict[letters[i]] = i+1
+    result = [dict[i] for i in s]
+    return sum(result)
+
