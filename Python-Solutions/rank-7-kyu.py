a833e806cb5ad27b64b8be88991163186afc41c3
William Smith <mr.williamjdsmith@gmail.com>
2022-08-02 23:44:40 -0700

    Added Kata: String ends with?

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -560,3 +560,15 @@ def words_to_marks(s):
     result = [dict[i] for i in s]
     return sum(result)
 
+"""
+Kata: String ends with?
+Rank: 7 kyu
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+"""
+def solution(string, ending):
+    return True if string[len(string) - len(ending):] == ending else False
+
