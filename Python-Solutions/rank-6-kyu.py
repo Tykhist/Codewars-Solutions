e41b5451bbe6d70c79c7be48eef6aece1535d402
William Smith <mr.williamjdsmith@gmail.com>
2022-08-04 20:43:20 -0700

    Added Kata: Twisted Sum

--- Python-Solutions/rank-6-kyu.py
+++ Python-Solutions/rank-6-kyu.py
@@ -379,3 +379,21 @@ def persistence(n):
         count += 1
         numstr = str(temp)
     return count
+
+"""
+Kata: Twisted Sum
+Rank: 6 kyu
+Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+Examples
+# N = 4
+1 + 2 + 3 + 4 = 10
+# N = 10
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+# N = 12
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+"""
+def compute_sum(n):
+    nums = "".join([str(i) for i in range(n+1)])
+    return sum([int(i) for i in nums])
+
