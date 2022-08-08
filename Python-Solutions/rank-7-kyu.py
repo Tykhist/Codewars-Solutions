83b13c008afd7560ebc2142e878e3d94088c9f4d
William Smith <mr.williamjdsmith@gmail.com>
2022-08-07 21:25:41 -0700

    Added Kata: Smallest value of an array

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -593,3 +593,21 @@ def switcheroo(s):
             result += i
     return result
 
+"""
+Kata: Smallest value of an array
+Rank: 7 kyu
+Write a function that can return the smallest value of an array or the index of that value. 
+The function's 2nd parameter will tell whether it should return the value or the index.
+Assume the first parameter will always be an array filled with at least 1 number and no duplicates. 
+Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+min([1,2,3,4,5], 'value') // => 1
+min([1,2,3,4,5], 'index') // => 0
+"""
+def find_smallest(numbers,to_return):
+    if to_return == 'value':
+        return min(numbers)
+    else:
+        return numbers.index(min(numbers))Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+
