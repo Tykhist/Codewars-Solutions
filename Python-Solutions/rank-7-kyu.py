61e28042bb5be39994322d691301d50f6b74889f
William Smith <mr.williamjdsmith@gmail.com>
2022-08-09 22:56:24 -0700

    Added Kata: Jaden Casing Strings

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -617,3 +617,22 @@ Write reverseList function that simply reverses lists.
 """
 def reverse_list(lst):
     return lst[::-1]
+
+"""
+Kata: Jaden Casing Strings
+Rank: 7 kyu
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known 
+for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how 
+contractions are expected to be in the example below. Your task is to convert strings to how they would be written 
+by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+Link to Jaden's former Twitter account @officialjaden via archive.org
+"""
+def to_jaden_case(string):
+    return " ".join([i[0].upper() + i[1:].lower() for i in string.split(" ")]) if string else ""
+
