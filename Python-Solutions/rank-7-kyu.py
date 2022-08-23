2890f1781629f7124741d195a19aa05746a2ebfc
William Smith <mr.williamjdsmith@gmail.com>
2022-08-23 02:24:19 -0700

    Added Kata: Small enough? - Beginner

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -699,4 +699,14 @@ Explanation:
 def min_value(digits):
     return int("".join(sorted({str(i) for i in digits})))
 
-
+"""
+Kata: Small enough? - Beginner
+Rank: 7 kyu
+You will be given an array and a limit value. You must check that all values in the array are below 
+or equal to the limit value. If they are, return true. Else, return false.
+You can assume all values in the array are numbers.
+"""
+def small_enough(array, limit):
+    for i in array:
+        if i > limit: return False 
+    return True
