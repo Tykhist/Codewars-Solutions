a5f8e82a94cdb0ff9505239375ec45d33045ca94
William Smith <mr.williamjdsmith@gmail.com>
2022-09-05 20:48:11 -0700

    Added Kata: Stanton measure

--- Python-Solutions/rank-7-kyu.py
+++ Python-Solutions/rank-7-kyu.py
@@ -710,3 +710,18 @@ def small_enough(array, limit):
     for i in array:
         if i > limit: return False 
     return True
+
+"""
+Kata: Stanton measure
+Rank: 7 kyu
+The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. 
+Let this count be n. The Stanton measure is the number of times that n appears in the array.
+Write a function which takes an integer array and returns its Stanton measure.
+
+Examples
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
+"""
+def stanton_measure(arr):
+    x = arr.count(1)
+    return arr.count(x)
